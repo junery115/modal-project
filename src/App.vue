5c7e10af7e8589897bf1374652e5590237fa4f46
Japheth Awah <junery115@gmail.com>
2024-04-15 12:54:56 +0100

    adding modal and talking about multiple componets

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <h1>{{ title }}</h1>
+  <Modal />
   <input types="text" ref="name">
   <button @click="handleClick">click me</button>
 </template>
 
 <script>
-
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  components: {
+    Modal
+  },
   data(){
     return{
       title: "My first view app :)"
